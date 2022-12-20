45b2e8c067357a1b034ae0476074aa4956fc39ab
Justin Allbright <JustinOtherAllbright@gmail.com>
2022-12-19 23:32:48 -0500

    add poggiers

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import App from "./react.js"
+
+const root = ReactDOM.createRoot(document.getElementById('ReactTest'));
+root.render(
+<React.Fragment>
+<App/>
+<App/>
+<App/>
+<App/>
+</React.Fragment>
+
+);
+
---
+++ src/react.js
@@ -0,0 +1,11 @@
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <h1>hello</h1>
+        )
+    }
+}
