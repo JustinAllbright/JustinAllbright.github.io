4971b10f6a3b07ee05d3a0dc32622067b35a1666
Justin Allbright <JustinOtherAllbright@gmail.com>
2022-12-23 00:22:32 -0500

    css changes

--- src/Components/Projects/Quotes/quotes.js
+++ src/Components/Projects/Quotes/quotes.js
@@ -3,28 +3,33 @@ export default class Quotes extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            QuoteNumber: Math.floor(Math.random() * (5))
+            QuoteNumber: Math.floor(Math.random() * (6))
         }
         this.increment = this.increment.bind(this);
     }
 
     increment() {
             this.setState({
-            QuoteNumber: Math.floor(Math.random() * (5))
+            QuoteNumber: Math.floor(Math.random() * (6))
             })
     }
 
     render() {
-        const QuotesList = ['he dodged the bullet like how women dodge me',
-        'oligarchy? yeah, thats what they call the manager of olive garden',
-        '9+4 is even',
-        'khakis are the new bitcoin',
-        'woah dude outside theres like a reverse sun set! ...no wait thats a sunrise FUCK']
+        const QuotesList = [['he dodged the bullet like how women dodge me', 'Christian Huff'],
+        ['oligarchy? yeah, thats what they call the manager of olive garden', 'Christian Huff'],
+        ['9+4 is even', 'Jeff'],
+        ['khakis are the new bitcoin', 'Christian Huff'],
+        ['woah dude outside theres like a reverse sun set! ...no wait thats a sunrise FUCK', "Clayton Bonigut"],
+        ['i cant really snack and eat', 'Vinny Gefre']]
+
         return(
             <center>
-            <div id="quote-box">
-                <p id="text">{QuotesList[this.state.QuoteNumber]}</p>
-                <p id="author">-Author</p>
+            <div id="quote-box"
+            style={{
+                height: 10 + "%"
+            }}>
+                <p id="text">{QuotesList[this.state.QuoteNumber][0]}</p>
+                <p id="author">{QuotesList[this.state.QuoteNumber][1]}</p>
                 <button onClick={this.increment} id="new-quote">New Quote</button>
                 <a id="tweet-quote">Tweet</a>
 
